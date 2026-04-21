5b2911687f173bdbe2b6eba84c091c34855ccd4b
hunted104 <mutugian25@gmail.com>
2026-04-21 12:24:42 -0700

    update PostList
    Refactored PostList component to use a posts array for rendering PostCard components dynamically.

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,22 +1,36 @@
-import PostCard from './PostCard'
+import PostCard from './PostCard';
 
 function PostList() {
+  const posts = [
+    {
+      id: 1,
+      title: "Getting Started with React",
+      excerpt: "Learn the basics of React...",
+      author: "Ian",
+      date: "Jan 15, 2026"
+    },
+    {
+      id: 2,
+      title: "JavaScript Best Practices",
+      excerpt: "Write cleaner code...",
+      author: "Jane Smith",
+      date: "Jan 10, 2026"
+    }
+  ];
+
   return (
     <div className="post-list">
-      <PostCard 
-        title="Getting Started with React"
-        excerpt="Learn the basics of React..."
-        author="Ian Mutugi"
-        date="Jan 15, 2026"
-      />
-      <PostCard 
-        title="JavaScript Best Practices"
-        excerpt="Write cleaner code..."
-        author="Jane Smith"
-        date="Jan 10, 2026"
-      />
+      {posts.map(post => (
+        <PostCard
+          key={post.id}
+          title={post.title}
+          excerpt={post.excerpt}
+          author={post.author}
+          date={post.date}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default PostList
+export default PostList;
