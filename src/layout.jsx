fa1a8437668dc9ca4404e36875a9b89d4881dae6
hunted104 <mutugian25@gmail.com>
2026-04-22 13:25:37 -0700

    Refactor Layout component formatting

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,22 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
 function Layout({ children }) {
-    return (
-        <div className="layout">
-            <Header />
-            <main className="main-content">
-                {children}
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="layout">
+      <Header />
+      <main className="main-content">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-// Usage
-function App() {
-    return (
-        <Layout>
-            <h1>Home Page</h1>
-            <PostList />
-        </Layout>
-    );
-}
 export default Layout;
