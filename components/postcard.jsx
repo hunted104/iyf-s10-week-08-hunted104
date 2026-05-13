5ea8bb13e1889b745df795ff1ae202d58620c7d7
hunted104 <mutugian25@gmail.com>
2026-05-13 11:32:21 -0700

    Update postcard.jsx

--- components/postcard.jsx
+++ components/postcard.jsx
@@ -1,36 +1,10 @@
-import PostCard from './PostCard';
-
-function PostList() {
-    const posts = [
-        {
-            id: 1,
-            title: "Getting Started with React",
-            excerpt: "Learn the basics of React...",
-            author: "Ian Mutugi",
-            date: "Jan 15, 2026"
-        },
-        {
-            id: 2,
-            title: "JavaScript Best Practices",
-            excerpt: "Write cleaner code...",
-            author: "Jane Smith",
-            date: "Jan 10, 2026"
-        }
-    ];
-
-    return (
-        <div className="post-list">
-            {posts.map(post => (
-                <PostCard
-                    key={post.id}
-                    title={post.title}
-                    excerpt={post.excerpt}
-                    author={post.author}
-                    date={post.date}
-                />
-            ))}
-        </div>
-    );
+function PostCard({ title, author }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </div>
+  );
 }
 
-export default PostList;
+export default PostCard;
